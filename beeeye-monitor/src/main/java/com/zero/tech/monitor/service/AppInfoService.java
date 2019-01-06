3e54bc1028b220689c5b72a0b9582215c9c523b8
qr7973 <qr7972@gmail.com>
2019-01-06 12:34:42 +0800

    add config

--- beeeye-monitor/src/main/java/com/zero/tech/monitor/service/AppInfoService.java
+++ beeeye-monitor/src/main/java/com/zero/tech/monitor/service/AppInfoService.java
@@ -4,12 +4,18 @@ import com.zero.tech.base.constant.Constants;
 import com.zero.tech.base.constant.LogCollectionStatus;
 import com.zero.tech.data.db.domain.AppInfo;
 import com.zero.tech.data.db.mapper.AppInfoMapper;
+import com.zero.tech.monitor.listener.AppChildrenChangeListener;
 import org.I0Itec.zkclient.ZkClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class AppInfoService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AppChildrenChangeListener.class);
 
     @Autowired
     private ZkClient zkClient;
@@ -61,9 +67,16 @@ public class AppInfoService {
      * @param type
      */
     public void delete(String host, String app, int type) {
-        AppInfo appInfo = appInfoMapper.findAppInfoByPK(host, app, type).get(0);
-        if (null != appInfo) {
-            appInfoMapper.delete(appInfo);
+        List<AppInfo> appInfos = appInfoMapper.findAppInfoByPK(host, app, type);
+        if (appInfos != null) {
+            if (appInfos.size() == 1) {
+                AppInfo appInfo = appInfoMapper.findAppInfoByPK(host, app, type).get(0);
+                appInfoMapper.delete(appInfo);
+            } else if (appInfos.size() > 1) {
+                LOGGER.error("host {},app {},type {} exist repeatedly");
+            } else {
+                LOGGER.error("host {},app {},type {} does not exist");
+            }
         }
     }
 
